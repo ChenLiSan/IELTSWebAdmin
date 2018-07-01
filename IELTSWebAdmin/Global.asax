f77253e740e27f143b89776018414e409395918e
Chen Li San <33310582+ChenLiSan@users.noreply.github.com>
2018-07-01 13:12:35 +0800

    Add project files.

---
+++ IELTSWebAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IELTSWebAdmin.Global" Language="C#" %>
---
+++ IELTSWebAdmin/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IELTSWebAdmin.Startup))]
+namespace IELTSWebAdmin
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
